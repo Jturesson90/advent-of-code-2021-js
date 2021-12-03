fb3653377af149d46bf7f8d1399cc4785860b4ec
Jesper Turesson <Jturesson90@gmail.com>
2021-12-03 11:43:36 +0100

    Day2

--- src/days/Day02.js
+++ src/days/Day02.js
@@ -1,7 +1,46 @@
+import { convertToStringArray } from '../puzzleInputs'
+
 export const puzzleA = input => {
-    return input
+    var a = convertToStringArray(input)
+    const c = {
+        forward: 0,
+        down: 0,
+        up: 0,
+    }
+    for (let index = 0; index < a.length; index++) {
+        const element = a[index] + ''
+        var b = element.split(' ')
+        c[b[0]] = c[b[0]] + Number.parseInt(b[1])
+    }
+    return c.forward * (c.down - c.up)
 }
 
 export const puzzleB = input => {
-    return input
+    var a = convertToStringArray(input)
+
+    const c = {
+        horizontal: 0,
+        depth: 0,
+        aim: 0,
+    }
+    for (let index = 0; index < a.length; index++) {
+        const element = a[index] + ''
+        var b = element.split(' ')
+        let dir = b[0]
+        let amount = Number.parseInt(b[1])
+        c[dir] += amount
+        switch (dir) {
+            case 'forward':
+                c.horizontal += amount
+                c.depth += amount * c.aim
+                break
+            case 'up':
+                c.aim -= amount
+                break
+            case 'down':
+                c.aim += amount
+                break
+        }
+    }
+    return c.depth * c.horizontal
 }
--- test/Day02.test.js
+++ test/Day02.test.js
@@ -1,11 +1,39 @@
-import { puzzleA, puzzleB } from '../src/days/Day01'
+import { puzzleA, puzzleB } from '../src/days/Day02'
 import { getPuzzleInputText } from '../src/puzzleInputs'
 
-const day = '01'
+const day = '02'
 
 test('day02 puzzle a', () => {
     // Arrange
-
+    let expected = 2039912
+    var puzzleInput = getPuzzleInputText(day)
+    var actual = puzzleA(puzzleInput)
     // Assert
-    expect(0).toBe(0)
+    expect(expected).toBe(actual)
+})
+test('day02 puzzle a example', () => {
+    // Arrange
+    let expected = 150
+    var puzzleInput = getPuzzleInputText(day, 1)
+    var actual = puzzleA(puzzleInput)
+    // Assert
+    expect(expected).toBe(actual)
+})
+
+test('day02 puzzle b', () => {
+    // Arrange
+    let expected = 1942068080
+    var puzzleInput = getPuzzleInputText(day)
+    var actual = puzzleB(puzzleInput)
+    // Assert
+    expect(expected).toBe(actual)
+})
+
+test('day02 puzzle b example', () => {
+    // Arrange
+    let expected = 900
+    var puzzleInput = getPuzzleInputText(day, 1)
+    var actual = puzzleB(puzzleInput)
+    // Assert
+    expect(expected).toBe(actual)
 })
