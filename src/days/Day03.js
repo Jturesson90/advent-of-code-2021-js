6477a2dbcd3195e928f3ae19c5b806df3a09f4a1
Jesper Turesson <Jturesson90@gmail.com>
2021-12-03 11:43:44 +0100

    Day3
    Day3

--- src/days/Day03.js
+++ src/days/Day03.js
@@ -1,7 +1,40 @@
+import { convertToStringArray } from '../puzzleInputs'
 export const puzzleA = input => {
-    return input
+    let a = convertToStringArray(input)
+    let b = []
+    if (!Array.isArray(a)) return 0
+
+    for (let i = 0; i < a.length; i++) {
+        const element = a[i]
+        for (let j = 0; j < element.length; j++) {
+            const binary = element[j]
+            if (!b[j]) b[j] = []
+            b[j].push(Number.parseInt(binary))
+        }
+    }
+    let c = b.map(val => (val.filter(bin => bin === 1).length > val.length / 2 ? 1 : 0))
+    let d = c.map(val => (val === 0 ? 1 : 0)).join('')
+    let e = c.join('')
+    const gamma = Number.parseInt(e, 2)
+    const epsilon = Number.parseInt(d, 2)
+    return gamma * epsilon
 }
 
 export const puzzleB = input => {
-    return input
+    let a = convertToStringArray(input)
+    const oxygen = calc(a, 0, (ones, len) => (ones >= len ? '1' : '0'))
+    const CO2 = calc(a, 0, (ones, len) => (ones < len ? '1' : '0'))
+    return oxygen * CO2
+}
+const calc = (arr, index, callbackFn) => {
+    if (!Array.isArray(arr)) return 0
+    if (arr.length === 1) {
+        const decimal = Number.parseInt(arr.join(''), 2)
+        return decimal
+    }
+    const len = arr.length / 2
+    const onesNum = arr.filter(a => a[index] === '1').length
+    const chosenNumber = callbackFn(onesNum, len)
+    const filteredArray = arr.filter(a => a[index] === chosenNumber)
+    return calc(filteredArray, index + 1, callbackFn)
 }
--- test/Day03.test.js
+++ test/Day03.test.js
@@ -4,8 +4,19 @@ import { getPuzzleInputText } from '../src/puzzleInputs'
 const day = '03'
 test('day03 puzzle a', () => {
     // Arrange
-    const expected = ''
+    const expected = 3882564
     const puzzleInput = getPuzzleInputText(day)
+    // Act
+    const result = puzzleA(puzzleInput)
+
+    // Assert
+    expect(result).toBe(expected)
+})
+
+test('day03 puzzle a example 1', () => {
+    // Arrange
+    const expected = 198
+    const puzzleInput = getPuzzleInputText(day, 1)
     // Act
     const result = puzzleA(puzzleInput)
 
@@ -14,8 +25,18 @@ test('day03 puzzle a', () => {
 })
 test('day03 puzzle b', () => {
     // Arrange
-    const expected = ''
+    const expected = 3385170
     const puzzleInput = getPuzzleInputText(day)
+    // Act
+    const result = puzzleB(puzzleInput)
+
+    // Assert
+    expect(result).toBe(expected)
+})
+test('day03 puzzle b example 1', () => {
+    // Arrange
+    const expected = 230
+    const puzzleInput = getPuzzleInputText(day, 1)
     // Act
     const result = puzzleB(puzzleInput)
 
