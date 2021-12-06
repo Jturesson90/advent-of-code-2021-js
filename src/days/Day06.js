d62fbf23f2695485700d018d193bea75ce84f0f0
Jesper Turesson <Jturesson90@gmail.com>
2021-12-06 10:16:29 +0100

    Day 6

--- src/days/Day06.js
+++ src/days/Day06.js
@@ -1,7 +1,38 @@
-export const puzzleA = input => {
-    return input
+import { convertToNumberArray } from '../puzzleInputs'
+
+export const puzzleA = ({ input, days }) => {
+    const arr = (input + '').split(',')
+    let arrayCopy = [...arr]
+    if (!Array.isArray(arrayCopy)) return 0
+    for (let i = 0; i < days; i++) {
+        const toBeAdded = []
+        for (let j = 0; j < arrayCopy.length; j++) {
+            if (arrayCopy[j] === 0) {
+                arrayCopy[j] = 6
+                toBeAdded.push(8)
+            } else {
+                arrayCopy[j] = arrayCopy[j] - 1
+            }
+        }
+        arrayCopy = arrayCopy.concat(toBeAdded)
+        console.log(`Day ${i + 1}: ${arrayCopy.length}`)
+    }
+    return arrayCopy.length
+}
+export const puzzleB = ({ input, days }) => {
+    const arr = convertToNumberArray(input, ',')
+    if (!Array.isArray(arr)) return 0
+    return countLanternfish(arr, 256)
 }
 
-export const puzzleB = input => {
-    return input
+const countLanternfish = (initialState, days, rate = 7) => {
+    const numFish = new Array(rate + 2).fill(0)
+    initialState.forEach(n => numFish[n]++)
+    while (days-- > 0) {
+        const fishToDouble = numFish[0]
+        numFish.slice(0, -1).forEach((_, i) => (numFish[i] = numFish[i + 1]))
+        numFish[rate - 1] += fishToDouble
+        numFish[rate + 1] = fishToDouble
+    }
+    return numFish.reduce((a, b) => a + b, 0)
 }
--- test/Day06.test.js
+++ test/Day06.test.js
@@ -2,22 +2,34 @@ import { puzzleA, puzzleB } from '../src/days/Day06'
 import { getPuzzleInputText } from '../src/puzzleInputs'
 
 const day = '06'
-test('day06 puzzle a', () => {
+
+test('day06 puzzle a example 1', () => {
     // Arrange
-    const expected = ''
-    const puzzleInput = getPuzzleInputText(day)
+    const expected = 26
+    const puzzleInput = getPuzzleInputText(day, 1)
     // Act
-    const result = puzzleA(puzzleInput)
+    const result = puzzleA({ input: puzzleInput, days: 18 })
 
     // Assert
     expect(result).toBe(expected)
 })
-test('day06 puzzle b', () => {
+test('day06 puzzle a', () => {
     // Arrange
-    const expected = ''
+    const expected = 380758
     const puzzleInput = getPuzzleInputText(day)
     // Act
-    const result = puzzleB(puzzleInput)
+    const result = puzzleA({ input: puzzleInput, days: 80 })
+
+    // Assert
+    expect(result).toBe(expected)
+})
+
+test('day06 puzzle b', () => {
+    // Arrange
+    const expected = 1710623015163
+    const puzzleInput = getPuzzleInputText(day)
+    // Act
+    const result = puzzleB({ input: puzzleInput, days: 256 })
 
     // Assert
     expect(result).toBe(expected)
