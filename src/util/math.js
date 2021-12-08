51d9b15b31cba18e61497b604d61bf10735b7fd3
Jesper Turesson <Jturesson90@gmail.com>
2021-12-08 09:16:02 +0100

    Update math.js

--- src/util/math.js
+++ src/util/math.js
@@ -2,24 +2,13 @@ export const triangleNumber = (num, cache = {}) => {
     if (num === 1) return 1
     else if (num < 0) return -1
     else if (num === 0) return 0
-    else if (cache[num] !== undefined) {
-        return cache[num]
-    } else {
-        const a = num + triangleNumber(num - 1, cache)
-        cache[num] = a
-        return a
-    }
+    else if (cache[num] !== undefined) return cache[num]
+    return (cache[num] = num + triangleNumber(num - 1, cache))
 }
 
 export const factorial = (num, cache = {}) => {
-    if (cache[num] !== undefined) {
-        return cache[num]
-    }
+    if (cache[num] !== undefined) return cache[num]
     if (num < 0) return -1
     else if (num == 0) return 1
-    else {
-        const a = num * factorial(num - 1, cache)
-        cache[num] = a
-        return a
-    }
+    return (cache[num] = num * factorial(num - 1, cache))
 }
