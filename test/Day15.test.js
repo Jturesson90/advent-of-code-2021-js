051e3069e5f3c2a77d327c429ef728c3b677f1f0
Jesper Turesson <Jturesson90@gmail.com>
2021-12-21 16:10:51 +0100

    Day 15

--- test/Day15.test.js
+++ test/Day15.test.js
@@ -4,7 +4,7 @@ import { getPuzzleInputText } from '../src/puzzleInputs'
 const day = '15'
 test('day15 puzzle a', () => {
     // Arrange
-    const expected = ''
+    const expected = 366
     const puzzleInput = getPuzzleInputText(day)
     // Act
     const result = puzzleA(puzzleInput)
@@ -12,10 +12,32 @@ test('day15 puzzle a', () => {
     // Assert
     expect(result).toBe(expected)
 })
+test('day15 puzzle a example 1', () => {
+    // Arrange
+    const expected = 40
+    const puzzleInput = getPuzzleInputText(day, 1)
+    // Act
+    const result = puzzleA(puzzleInput)
+
+    // Assert
+    expect(result).toBe(expected)
+})
+
+315
 test('day15 puzzle b', () => {
     // Arrange
-    const expected = ''
+    const expected = 2829
     const puzzleInput = getPuzzleInputText(day)
+    // Act
+    const result = puzzleB(puzzleInput)
+
+    // Assert
+    expect(result).toBe(expected)
+})
+test('day15 puzzle b example 1', () => {
+    // Arrange
+    const expected = 315
+    const puzzleInput = getPuzzleInputText(day, 1)
     // Act
     const result = puzzleB(puzzleInput)
 
